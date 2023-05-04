747b87c003c067a46054fdeb5785cf5366812481
abhiavad <abhiavad@gmail.com>
2023-05-04 17:50:20 +0530

    arduino to excel completed

--- sun_main.py
+++ sun_main.py
@@ -4,35 +4,48 @@ import serial
 
 # Configure the serial port
 ser = serial.Serial('COM3', 9600, timeout=1)
-
 # Define the data structure
 data = []
-i=0;
+i=0
 # Loop to read and store data every second
 while True:
-    # Get the current time
-    timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+    if i==0:
+         # Get the current time
+        timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+        # Read the sensor data from the Arduino
+        #ser.write(b'r\n')
+        response = ser.readline()
+        print('\n',response,'\n')
 
-    # Read the sensor data from the Arduino
-    ser.write(b'r')
-    response = ser.readline().decode().strip()
-    x, y, z = response.split(',')
-    x = float(x)
-    y = float(y)
-    z = float(z)
+        time.sleep(1)
+        i=i+1
+    else:
+        # Get the current time
+        timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+        # Read the sensor data from the Arduino
+        #ser.write(b'r\n')
+        response = ser.readline().decode("utf-8").strip()
+        print('\n',response,'\n')
+        x, y, z = response.split(',')
+        x = float(x)
+        y = float(y)
+        z = float(z)
+        
+        i=i+1;
 
-    #store the data with a timestamp
-    data.append([timestamp, x, y, z])
+        if x%(0.5)<=0.01 and i<200:
+            #store the data with a timestamp
+             data.append([timestamp, x, y, z])
+             # Wait for 10 milliseconds before the next reading
+             time.sleep(0.01)
+        elif i>=200:
+            # Save the data to an Excel file
+            df = pd.DataFrame(data, columns=['Timestamp', 'X', 'Y', 'Z'])
+            df.to_excel("sensor_data.xlsx", index=False)
+            ser.close()
+            exit()
 
-    # Wait for 10 milliseconds before the next reading
-    time.sleep(0.01)
-    i+=1
-    if x%(0.5)<=0.01 and i<50000:
-        # Save the data to an Excel file
-        df = pd.DataFrame(data, columns=['Timestamp', 'X', 'Y', 'Z'])
-        df.to_excel('sensor_data.xlsx', index=False)
+        
 
-    
-
-    
+        
     
